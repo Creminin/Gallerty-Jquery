6fbbbb86784768ed437b3f6525831a614be38656
Nathan Lucas <n54318879@gmail.com>
2022-12-26 16:23:48 -0300

    Final

--- main.js
+++ main.js
@@ -9,5 +9,20 @@ $(document).ready(function () {
 
   $('form').on('submit', (e) => {
     e.preventDefault();
+    const novaImagem = $('#LinkImg').val();
+    const novoItem = $('<li></li>');
+
+    $(`<img src="${novaImagem}" />`).appendTo(novoItem);
+
+    $(`<div class="Link-Total-Size-Img">
+        <a 
+        href="${novaImagem}"
+        target="_blank"
+        title="Abrir imagem em tamanho real"
+        >Abrir imagem em tamanho real</a>
+    </div>`).appendTo(novoItem);
+    $(novoItem).appendTo('ul')
+
+    $('#LinkImg').val('');
   })
 })
